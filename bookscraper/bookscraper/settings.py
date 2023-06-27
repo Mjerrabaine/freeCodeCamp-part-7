49ab1ebe0375ccd04593417a15b3d676e9d9e815
Mjerrabaine <85709451+Mjerrabaine@users.noreply.github.com>
2023-06-27 14:29:19 -0500

    Update settings.py

--- bookscraper/bookscraper/settings.py
+++ bookscraper/bookscraper/settings.py
@@ -12,6 +12,7 @@ BOT_NAME = 'bookscraper'
 SPIDER_MODULES = ['bookscraper.spiders']
 NEWSPIDER_MODULE = 'bookscraper.spiders'
 
+SCRAPEOPS_API_KEY = '5b86950c-ecb2-4019-b1bb-93a0a28ff75a'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'bookscraper (+http://www.yourdomain.com)'
@@ -54,11 +55,19 @@ ROBOTSTXT_OBEY = True
 #    'bookscraper.middlewares.BookscraperDownloaderMiddleware': 543,
 #}
 
+DOWNLOADER_MIDDLEWARES = {
+    'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+}
+
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
+EXTENSIONS = {
+    'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+}
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
